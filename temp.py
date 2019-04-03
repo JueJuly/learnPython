09fe8e2ee124e8aefc831ac2631c52b2b1ced487
wszlzhu <wszlzhu@sunnyoptical.com>
2019-04-03 16:37:44 +0800

    显示4中激励函数code

--- temp.py
+++ temp.py
@@ -4,20 +4,47 @@ Spyder Editor
 
 This is a temporary script file.
 """
-import os
+#import os
 import torch
 from torch.autograd import Variable #torch Variable module
+import torch.nn.functional as F
 #import torchvision
 import numpy as np
+import matplotlib.pyplot as plt
 
-# 先生鸡蛋
-tensor = torch.FloatTensor([[1,2],[3,4]])
-# 把鸡蛋放到篮子里, requires_grad是参不参与误差反向传播, 要不要计算梯度
-variable = Variable(tensor, requires_grad=True)
+#generate some data for test
+x = torch.linspace(-5,5,200)
+x = Variable(x)
 
-t_out = torch.mean(tensor*tensor)
-v_out = torch.mean(variable*variable)
+x_np = x.data.numpy() #convert to numpy array, for showing
 
-print(t_out)
+#some Activation Function
+y_relu      = F.relu(x).data.numpy()
+y_sigmoid   = F.sigmoid(x).data.numpy()
+y_tanh      = F.tanh(x).data.numpy()
+y_softplus  = F.softplus(x).data.numpy()
 
-print(v_out)
+ 
+plt.figure(1,figsize=(8,6))
+
+plt.subplot(2,2,1)
+plt.plot(x_np,y_relu,c='red',label='relu')
+plt.ylim(-1,5)
+plt.legend(loc='best')
+
+plt.subplot(222)
+plt.plot(x_np,y_sigmoid,c='red',label='sigmoid')
+plt.ylim(-0.2,1.2)
+plt.legend(loc='best')
+
+plt.subplot(223)
+plt.plot(x_np,y_tanh,c='red',label='tanh')
+plt.ylim(-1.2,1.2)
+plt.legend(loc='best')
+
+plt.subplot(224)
+plt.plot(x_np,y_softplus,c='red',label='softplus')
+plt.ylim(-0.2,6.0)
+plt.legend(loc='best')
+
+plt.show()
